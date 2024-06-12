80916f4018aa2ade6065e09a401904405e1a25ed
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2024-06-12 16:32:49 -0600

    First commit

---
+++ src/main/java/com/fonyou/pruebaTecnica_examen_fonYou_back/PruebaTecnicaExamenFonYouBackApplication.java
@@ -0,0 +1,13 @@
+package com.fonyou.pruebaTecnica_examen_fonYou_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaExamenFonYouBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaExamenFonYouBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fonyou/pruebaTecnica_examen_fonYou_back/PruebaTecnicaExamenFonYouBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonyou.pruebaTecnica_examen_fonYou_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaExamenFonYouBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
